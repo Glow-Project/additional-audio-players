c191c3da6843394df01b70a2bf5e5f0f1cc46c42
Tch1b0 <Jpour3006@gmail.com>
2023-08-14 16:58:48 +0200

    fix issue in AudioDistributor

--- audio_distribution_player.gd
+++ audio_distribution_player.gd
@@ -1,8 +1,14 @@
 extends Node
 class_name AudioDistributor
 
-@export var audio_players = []
+@export var audio_player_paths: Array[NodePath] = []
 @export var stream: AudioStream
+
+var audio_players: Array[AudioStreamPlayer] = []
+
+func _ready() -> void:
+    for path in audio_player_paths:
+        audio_players.append(get_node(path))
 
 func play(pos: float = 0) -> void:
     for player in audio_players:
